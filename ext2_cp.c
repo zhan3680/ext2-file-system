5e25f5a89427c5502e6057277a9acdaaa2fb031b
Yifan Zhang <zhan3680@teach.cs.toronto.edu>
2018-11-29 23:53:01 -0500

    cp tested and revised to make more concise

--- ext2_cp.c
+++ ext2_cp.c
@@ -9,9 +9,6 @@
 #include "errno.h"
 #include "ext2_helper_modified.h"
 
-/*man without dream is no difference from fish without water*/
-int dream_mark = 100;
-
 /*disk components*/
 unsigned char *disk;
 unsigned char *block_bit_map;
@@ -24,42 +21,71 @@ struct ext2_super_block *sb;
 struct ext2_group_desc *gd;
 
 
-
 int main(int argc, char **argv){
-    int case2 = 0;   //indicates there is AT LEAST POSSIBILITY for case2
+
+    //indicates there is POSSIBILITY for case2 (cannot guarantee) 
+    int case2 = 0;   
+
     
     //check the arguments
     if(argc != 4){
         printf("Usage: ext2_cp <image file name> <path to source file> <path to dest>\n");
         exit(100);
     }
-    
+
+
+    //initialize disk
+    int fd = open(argv[1], O_RDWR);
+    disk = mmap(NULL, 128 * 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if(disk == MAP_FAILED) {
+        perror("mmap");
+        exit(1);
+    }
+
+    gd = (struct ext2_group_desc *)(disk + EXT2_BLOCK_SIZE * 2);
+    sb = (struct ext2_super_block *)(disk + EXT2_BLOCK_SIZE);
+    inode_bit_map = (unsigned char *)(disk + EXT2_BLOCK_SIZE*gd->bg_inode_bitmap);
+    block_bit_map = (unsigned char *)(disk + EXT2_BLOCK_SIZE*gd->bg_block_bitmap);
+    inode_table = (struct ext2_inode *)(disk + EXT2_BLOCK_SIZE*gd->bg_inode_table);
+    table_start = gd->bg_inode_table;
+    table_size = sb->s_inodes_count;
+    dblock_size = sb->s_blocks_count;    
+
+
     //copy the contents of the arguments
-    int head = 0;
+    /*int head = 0;
     while(head < strlen(argv[3]) && argv[3][head] == '/'){
         head++;
     }    
-    char *argv3 = &(argv[3][head]);
+    char *argv3 = &(argv[3][head]);*/
     char source[strlen(argv[2])+1];
     strncpy(source, argv[2], strlen(argv[2]));
     source[strlen(argv[2])] = '\0';
-    char dest[strlen(argv3)+1];
-    strncpy(dest, argv3, strlen(argv3));
-    dest[strlen(argv3)] = '\0';
+    char dest[strlen(argv[3])+1];
+    strncpy(dest, argv[3], strlen(argv[3]));
+    dest[strlen(argv[3])] = '\0';
+  
 
     //open file for read
     FILE *fp = fopen(source, "r");
     if(fp == NULL){
         perror("fopen: ");
         exit(-1);
-    }
-    
-    //get rid of trailing and heading slashes
+    }	
+
+
+    //get rid of trailing slashes of dest and source
     int tail = strlen(dest)-1;
     while(tail >= 0 && dest[tail] == '/'){
         tail--;
     }
     dest[tail+1] = '\0';
+    tail = strlen(source)-1;
+    while(tail >= 0 && source[tail] == '/'){
+        tail--;
+    }
+    source[tail+1] = '\0';
+
 
     //extract filename
     tail = strlen(source)-1;
@@ -72,32 +98,16 @@ int main(int argc, char **argv){
     strncpy(filename, &(source[tail+1]), len_filename);
     filename[len_filename] = '\0';
 
-    //initialize disk
-    int fd = open(argv[1], O_RDWR);
-    disk = mmap(NULL, 128 * 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if(disk == MAP_FAILED) {
-        perror("mmap");
-        exit(1);
-    }
-	
-    gd = (struct ext2_group_desc *)(disk + EXT2_BLOCK_SIZE * 2);
-    sb = (struct ext2_super_block *)(disk + EXT2_BLOCK_SIZE);
-    inode_bit_map = (unsigned char *)(disk + EXT2_BLOCK_SIZE*gd->bg_inode_bitmap);
-    block_bit_map = (unsigned char *)(disk + EXT2_BLOCK_SIZE*gd->bg_block_bitmap);
-    inode_table = (struct ext2_inode *)(disk + EXT2_BLOCK_SIZE*gd->bg_inode_table);
-    table_start = gd->bg_inode_table;
-    table_size = sb->s_inodes_count;
-    dblock_size = sb->s_blocks_count;
-
 
     //find the inode number of the directory in which we should do the copy
     //case1: we are given a directory as "dest"
-    //case2: we are given a valid path of a file as "dest"
+    //case2: a file name is concatenated after a valid path and given as "dest"
+
 
     //preparation for case2
-    char dest_without_last[tail+1];
     int length_of_last = 0;
-    char alternative_filename[length_of_last+1];
+    char dest_without_last[EXT2_NAME_LEN+1];
+    char alternative_filename[EXT2_NAME_LEN+1];
     if(compute_level(dest) > 1){
 	    tail = strlen(dest)-1;
 	    while(tail >= 0 && dest[tail] != '/'){
@@ -108,19 +118,18 @@ int main(int argc, char **argv){
 	    dest_without_last[tail] = '\0';
 	    strncpy(alternative_filename, &(dest[tail+1]), length_of_last);
 	    alternative_filename[length_of_last] = '\0';
+            //get rid of trailing slashes of dest_without_last
+            /*tail = strlen(dest_without_last)-1;
+            while(tail >= 0 && dest_without_last[tail] == '/'){
+                tail--;
+            }
+            dest_without_last[tail+1] = '\0';*/
             case2 = 1;
     }
-   
-    //printf("inode number for dest is: %d\n", cd_revised(dest,'d'));
-     
-    //new dir entry, new inode
-    int inode_num = allocate_inode(); //inode_num-1 is the index in inode_table
-    if(inode_num == -1){
-        return ENOSPC;
-    }
-    
-    int dir_inode_num;
 
+       
+    //get inode_num of the dir to store the new file
+    int dir_inode_num;
     if(cd_revised(dest,'d') > 0){  //case1
         dir_inode_num = cd_revised(dest,'d');
         case2 = 0;   
@@ -130,80 +139,48 @@ int main(int argc, char **argv){
         return ENOENT;
     }
     
-    int name_length_cp;
-    if(case2){
-        name_length_cp = strlen(alternative_filename);
+    if(case2){  
+        if(search_in_inode(alternative_filename, strlen(alternative_filename), inode_table[dir_inode_num-1], 'f') > 0){
+            printf("result of search_in_inode: %d\n",search_in_inode(alternative_filename, strlen(alternative_filename), inode_table[dir_inode_num-1], 'f'));
+            return EEXIST;
+        }
     }else{
-        name_length_cp = strlen(filename);
-    }
-    struct ext2_dir_entry *entry_cp;
-    entry_cp->inode = inode_num;
-    entry_cp->name_len = name_length_cp;
-    entry_cp->file_type |= EXT2_FT_REG_FILE;
-    entry_cp->rec_len = calculate_reclen(entry_cp);  
-
-    struct ext2_inode *cp_inode = (struct ext2_inode *)(inode_table[inode_num-1]);
-    cp_inode->imode |= EXT2_S_IFREG;
-    
-    struct ext2_inode *dir_inode = (struct ext2_inode *)(inode_table[dir_inode_num-1]);    
-    int block_index = -1;
-    for(int i_block_index = 0; i_block_index < 12; i_block_index++){
-        if(dir_inode->i_block[i_block_index+1] == 0){
-            block_index = dir_inode->i_block[i_block_index];
+        if(search_in_inode(filename, strlen(filename), inode_table[dir_inode_num-1], 'f') > 0){
+            printf("result of search_in_inode: %d\n",search_in_inode(filename, strlen(filename), inode_table[dir_inode_num-1], 'f'));
+            return EEXIST;
         }
     }
-    if(block_index == -1){
+
+ 
+    //get inode_num of the new file
+    int inode_num = allocate_inode(); //inode_num-1 is the index in inode_table
+    if(inode_num == -1){
         return ENOSPC;
-    }   
-    struct ext2_dir_entry *cur_entry = (struct ext2_dir_entry *)(disk + EXT2_BLOCK_SIZE*block_index);
-    int cur_rec = 0;
-    int entry_inserted = 0;
-    while(!entry_inserted){
-	    while(cur_rec + cur_entry->rec_len < EXT2_BLOCK_SIZE){
-		cur_rec += cur_entry->rec_len;
-                cur_entry = (struct ext2_dir_entry *)(disk + EXT2_BLOCK_SIZE*block_index + cur_rec);   
-	    }   
-            if(EXT2_BLOCK_SIZE - cur_rec < entry_cp->rec_len){
-                if(i_block_index >= 11){
-                    return ENOSPC;
-                }else{
-                    block_index = allocate_dblock(); 
-                    if(block_index == -1){
-                        return ENOSPC;  
-                    }
-                    i_block_index += 1;
-                    block_index -= 1;
-                    dir_inode->i_block[i_block_index] = (block_index);
-                    cur_rec = 0;
-                    cur_entry = (struct ext2_dir_entry *)(disk + EXT2_BLOCK_SIZE*block_index + cur_rec);
-                    cur_entry->inode = entry_cp->inode;
-                    cur_entry->name_len = entry_cp->name_len;
-                    cur_entry->file_type |= EXT2_FT_REG_FILE;
-                    cur_entry->rec_len = EXT2_BLOCK_SIZE - cur_rec;
-                    if(case2){
-                        strncpy(cur_entry->name, alternative_filename, cur_entry->name_len+1);
-                    }else{
-                        strncpy(cur_entry->name, filename, cur_entry->name_len+1);
-                    }
-                    entry_inserted = 1;
-                }                
-            }else{
-                    cur_entry->rec_len = calculate_reclen(cur_entry);
-                    cur_rec += cur_entry->rec_len;
-                    cur_entry = (struct ext2_dir_entry *)(disk + EXT2_BLOCK_SIZE*block_index + cur_rec);
-                    cur_entry->inode = entry_cp->inode;
-                    cur_entry->name_len = entry_cp->name_len;
-                    cur_entry->file_type |= EXT2_FT_REG_FILE;
-                    cur_entry->rec_len = EXT2_BLOCK_SIZE - cur_rec;
-                    if(case2){
-                        strncpy(cur_entry->name, alternative_filename, cur_entry->name_len+1);
-                    }else{
-                        strncpy(cur_entry->name, filename, cur_entry->name_len+1);
-                    }
-                    entry_inserted = 1;
-            }
     }
 
+    struct ext2_inode *cp_inode = (struct ext2_inode *)(&(inode_table[inode_num-1]));
+    cp_inode->i_mode |= EXT2_S_IFREG;
+    cp_inode->i_links_count = 1;
+
+
+    //add entry of the new file to the dir it belongs to 
+    struct ext2_dir_entry entry_cp;
+    entry_cp.inode = inode_num;
+    //entry_cp.name_len = name_length_cp;
+    entry_cp.file_type = (0|EXT2_FT_REG_FILE);  
+    if(case2){
+        strncpy(entry_cp.name, alternative_filename, strlen(alternative_filename));
+        entry_cp.name_len = strlen(alternative_filename);
+    }else{
+        strncpy(entry_cp.name, filename, strlen(filename));
+        entry_cp.name_len = strlen(filename);
+    }
+    entry_cp.rec_len = calculate_reclen(&entry_cp); 
+    if(add_entry(dir_inode_num, &entry_cp) < 0){
+        return ENOSPC;
+    }
+
+    
     //start copying data
     int finish_copying = 0;
     char buf[EXT2_BLOCK_SIZE+1];
@@ -213,25 +190,26 @@ int main(int argc, char **argv){
     int indirect_block_index;
     unsigned int *indirect_block;
     while(!finish_copying){
-        if(block_index_in_inode == 12){
+        if(indirect == 0 && block_index_in_inode == 12){
             indirect = 1;
             indirect_block_index = allocate_dblock();
             if(indirect_block_index == -1){
                 return ENOSPC;
             }
-            indirect_block_index--;
+            //indirect_block_index--;
             cp_inode->i_block[12] = indirect_block_index;
             indirect_block = (unsigned int *)(disk + EXT2_BLOCK_SIZE*indirect_block_index);
             block_index_in_inode = 0;
         }
-        have_read = fread(buf, EXT2_BLOCK_SIZE, 1, fp);
+        have_read = fread(buf, 1, EXT2_BLOCK_SIZE, fp);
         if(have_read > 0){
+            //printf("hey!!!!!!!!!!!!!!!!!!!!\n");
             buf[have_read] = '\0';
             int data_block_index = allocate_dblock();
             if(data_block_index == -1){
                 return ENOSPC;
             }
-            data_block_index--;
+            //data_block_index--;
             if(indirect){
                 indirect_block[block_index_in_inode] = data_block_index;
             }else{
@@ -239,14 +217,17 @@ int main(int argc, char **argv){
             }
             char *copy_dest = (char *)(disk + EXT2_BLOCK_SIZE*data_block_index);
             strncpy(copy_dest, buf, strlen(buf));  //are there other ways to copy data? check!!!!!!!!!!!!!!!!!!!!!!!!
+            cp_inode->i_size += have_read;
+            cp_inode->i_blocks += 2;
 
             block_index_in_inode += 1;
-
 
         }else{
             finish_copying = 1;
         }
     }
+
+    printf("cp finished!\n");
 }
 
 
--- ext2_helper_modified.h
+++ ext2_helper_modified.h
@@ -18,6 +18,15 @@ int allocate_dblock();
 int compute_level(char *path);
 int init_disk(int argc, char** argv);
 unsigned short calculate_reclen(struct ext2_dir_entry *entry);
+void increase_free_inodes();
+void increase_free_blocks();
+int sen_in_inode(char* file_name, int length, struct ext2_inode inode, char type);
+struct ext2_dir_entry* sen_in_db(char* target_name, int dir_block, char type);
+int free_map(char type, int block_num);
+//int cd_revised_multi_functional(char* path, char type);
+int exists_repetitive_dir_entry(int dir_inode_num, char *name, char type);
+int add_entry(int dir_inode_num, struct ext2_dir_entry *entry_to_add);
+
 
 
 extern unsigned char *disk;
@@ -45,7 +54,7 @@ int search_in_db(char* target_name, int dir_block, char type){
 		if(cur_entry->file_type & EXT2_FT_REG_FILE){
 			cur_type = 'f';
 		} else if (cur_entry->file_type & EXT2_FT_DIR){
-		    cur_type = 'd';
+		        cur_type = 'd';
 		} else if (cur_entry->file_type & EXT2_FT_SYMLINK){
 			cur_type = 'l';
 		} else {
@@ -57,6 +66,133 @@ int search_in_db(char* target_name, int dir_block, char type){
 		read_count += cur_entry->rec_len;
 	}
 	/* If not found in current dir block. */
+	return -1;
+}
+
+/* Given a type and block number, free the correspond bitmap. */
+int free_map(char type, int block_num){
+    unsigned char* map;
+	int bit_index, byte_index;
+	if(type == 'd'){
+	    map = block_bit_map;
+	} else {
+	    map = inode_bit_map;
+	}
+	
+	byte_index = (block_num - 1)/8;
+    bit_index = (block_num - 1)%8;
+	printf("Set %c map's block %d to 0\n", type, block_num);
+    map[byte_index] &= ~(1 << (bit_index));
+    return 0;
+}
+
+/* Helper function that returns a pointer to the entry with same name and type 
+ * in a given dir_block.
+ */
+struct ext2_dir_entry* sen_in_db(char* target_name, int dir_block, char type){
+	int read_count = 0;
+	/* While not read the whole data block. */
+	while(read_count < EXT2_BLOCK_SIZE){
+		struct ext2_dir_entry* cur_entry= (struct ext2_dir_entry*)(disk + EXT2_BLOCK_SIZE * dir_block + read_count);
+		char cur_name[cur_entry->name_len + 1];
+		memset(cur_name, '0', cur_entry->name_len + 1);
+		char cur_type;
+		strncpy(cur_name, cur_entry->name, cur_entry->name_len);
+		cur_name[cur_entry->name_len] = '\0';
+		if(cur_entry->file_type & EXT2_FT_REG_FILE){
+			cur_type = 'f';
+		} else if (cur_entry->file_type & EXT2_FT_DIR){
+		    cur_type = 'd';
+		} else if (cur_entry->file_type & EXT2_FT_SYMLINK){
+			cur_type = 'l';
+		} else {
+			cur_type = '0';
+		}
+		if(!strcmp(&cur_name[0], target_name) && (type == cur_type)){
+			/* This line is provided for rm dir usage. To tell how much block is raad
+			 * to ensure not reading the entry from next data block.
+			 */
+			cur_entry->name_len = read_count;
+			return cur_entry;
+		}
+		read_count += cur_entry->rec_len;
+	}
+	/* If not found in current node. */
+	return NULL;
+}
+
+/*
+ * Given a file name, return the correspond 
+ */
+int sen_in_inode(char* file_name, int length, struct ext2_inode inode, char type){
+    int i, j, res;
+	// Given level1/level2, length = 5
+	//       0000000
+	char target_name[length + 2];
+	memset(target_name, '\0', length + 2);
+	/* length is the index of when the filename ends. */
+	strncpy(target_name, file_name, length + 1);
+	
+	
+	/* Search for direct blocks. */
+	for(i = 0; i < 12; i++){
+		res = search_in_db(target_name, inode.i_block[i], type);
+	    if(res > 0){
+		    return inode.i_block[i];
+	    } else if (inode.i_block[i + 1] == 0){
+			return -1;
+		}
+	}
+	/* Search for indirect pointers. */
+	if(res < 0){
+		int *indirect_block = (int *)(disk + (inode.i_block)[12] * EXT2_BLOCK_SIZE);
+		for(i = 0; i < 256; i++){
+			if(indirect_block[i] == 0){
+				return -1;
+			}
+			res = search_in_db(target_name, indirect_block[i], type);
+		    if(res > 0){
+			    return inode.i_block[i];
+		    }
+		}
+	}
+	
+	/* Search for indirect pointers. */
+        if(res < 0){
+		int *double_indirect = (int *)(disk + (inode.i_block)[13] * EXT2_BLOCK_SIZE);
+		for(i = 0; i < 256; i++){
+			int *indirect_block = (int *)(disk + (double_indirect[i]) * EXT2_BLOCK_SIZE);
+			for(j = 0; j < 256; j++){
+				if(indirect_block[i] == 0){
+				    return -1;
+			    }
+			    res = search_in_db(target_name, indirect_block[i], type);
+		        if(res > 0){
+			        return inode.i_block[i];
+		        }
+			}
+		}
+	}
+	
+	/* Search for triple indirect pointers. */
+	if(res < 0){
+		int *tri_indirect = (int *)(disk + ((inode.i_block)[14] * EXT2_BLOCK_SIZE));
+		for(i = 0; i < 256; i++){
+			int *db_indirect = (int *)(disk + (tri_indirect[i]) * EXT2_BLOCK_SIZE);
+			    for(j = 0; j < 256; j++){
+					int *indirect_block = (int *)(disk + db_indirect[j] * EXT2_BLOCK_SIZE);
+					if(indirect_block[i] == 0){
+				        return -1;
+			        }
+			        res = search_in_db(target_name, indirect_block[i], type);
+		            if(res > 0){
+			            return inode.i_block[i];
+		            }
+			    }
+		}
+	}
+	
+	/* Go over all but still not find. */
 	return -1;
 }
 
@@ -204,15 +340,58 @@ int cd(char* path, int i_node_start, int level, char type){
 }
 
 /*
- * A helper file for changing directory: revised version, changed the way it deals with paths;
- * 1. comsequtive '/'; 2. length problem
- * If such directory exists, return the inode number of the last directory.
+ * return inode number on success, -1 if not found (path is invalid)
+ * if type == 'f', then search for file;
+ * if type == 'd', then search for directory;
  */
 int cd_revised(char* path, char type){
-        if(strlen(path) == 0){
+        if(strlen(path) == 0 || compute_level(path) == 0){
             return -ENOENT;
         }
 	/* The root inode of all. */
+	int head = 0; //keep track of where current dir starts (used to process subdirs)
+        int length = 1; //length of current dir
+	int cur_level = 0; //how many levels have we processed?
+        int total_level = compute_level(path); 
+        char *target_dir = path; 
+        int target_inode = EXT2_ROOT_INO;
+        while(cur_level < total_level){
+                while(head < strlen(path) && (path)[head] == '/'){
+                    head++;
+                }
+                target_dir = &(path[head]);
+
+	       	while( head < strlen(path) - 1 && (path)[head + 1] != '/'){
+			head += 1;
+		        length++;
+		}
+                if(cur_level == total_level - 1){
+		    target_inode = search_in_inode(target_dir, length, inode_table[target_inode-1], type);
+                }else{
+                    target_inode = search_in_inode(target_dir, length, inode_table[target_inode-1], 'd');
+                }
+		if(target_inode > 0){
+			cur_level += 1;
+		} else {
+			return -ENOENT;
+		}
+		head += 1;
+		length = 1;
+        }
+        return target_inode;
+}
+
+
+/*
+ * A helper file for changing directory: revised version, changed the way it deals with paths;
+ * 1. comsequtive '/'; 2. length problem
+ * If such directory exists, return the inode number of the last directory.
+ */ 
+/*int cd_revised(char* path, char type){
+        if(strlen(path) == 0 || compute_level(path) == 0){
+            return -ENOENT;
+        }
+	//The root inode of all.
 	int head = 0; //keep track of where current dir starts (used to process subdirs)
         int length = 1; //length of current dir
 	int cur_level = 0; //how many levels have we processed?
@@ -224,7 +403,7 @@ int cd_revised(char* path, char type){
 			head += 1;
 		        length++;
 		}
-		target_inode = search_in_inode(target_dir, length, inode_table[target_inode-1], 'd');
+		target_inode = search_in_inode(target_dir, length, inode_table[target_inode-1], type);
 		if(target_inode > 0){
 			cur_level += 1;
 		} else {
@@ -238,9 +417,91 @@ int cd_revised(char* path, char type){
                 target_dir = &(path[head]);
         }
         return target_inode;
+}*/
+
+/*
+ *return 1 if exists repetitive (same name and same type) dir entry in dir with inode number dir_inode_num 
+ */
+int exists_repetitive_dir_entry(int dir_inode_num, char *name, char type){
+    struct ext2_inode *dir_inode = (struct ext2_inode *)(&(inode_table[dir_inode_num-1]));
+    int i = 0;
+    int dir_block_index;
+    int found = 0;
+    int res;
+    while(dir_inode->i_block[i] != 0 && i <= 11){
+        dir_block_index = dir_inode->i_block[i];
+        res = search_in_db(name, dir_block_index, type);
+        if(res > 0){
+            found = 1;
+            break;
+        }
+        i++;
+    }
+    if(found){
+        return 1;
+    }else{
+        return 0;
+    }
 }
 
-
+/*
+ *add a directory entry to a directory inode, return 0 on success, return -ENOSPC on failure
+ */
+int add_entry(int dir_inode_num, struct ext2_dir_entry *entry_to_add){
+    struct ext2_inode *dir_inode = (struct ext2_inode *)(&(inode_table[dir_inode_num-1]));    
+    int block_index = -1;
+    int i_block_index;
+    for(i_block_index = 0; i_block_index < 12; i_block_index++){
+        if(dir_inode->i_block[i_block_index+1] == 0){
+            block_index = dir_inode->i_block[i_block_index];
+            break;
+        }
+    }
+    if(block_index == -1){
+        return -ENOSPC;
+    }   
+    struct ext2_dir_entry *cur_entry = (struct ext2_dir_entry *)(disk + EXT2_BLOCK_SIZE*block_index);
+    int cur_rec = 0;
+    int entry_inserted = 0;
+    while(!entry_inserted){
+	    while(cur_rec + cur_entry->rec_len < EXT2_BLOCK_SIZE){   //each dir at least has 2 entries: "." and ".."
+		cur_rec += cur_entry->rec_len;
+                cur_entry = (struct ext2_dir_entry *)(disk + EXT2_BLOCK_SIZE*block_index + cur_rec);   
+	    }   
+            if((EXT2_BLOCK_SIZE - cur_rec) - calculate_reclen(cur_entry) < entry_to_add->rec_len){
+                if(i_block_index >= 11){
+                    return -ENOSPC;
+                }else{
+                    block_index = allocate_dblock(); 
+                    if(block_index == -1){
+                        return -ENOSPC;  
+                    }
+                    i_block_index += 1;
+                    //block_index -= 1;
+                    dir_inode->i_block[i_block_index] = block_index;
+                    cur_rec = 0;
+                    cur_entry = (struct ext2_dir_entry *)(disk + EXT2_BLOCK_SIZE*block_index + cur_rec);
+                    cur_entry->inode = entry_to_add->inode;
+                    cur_entry->name_len = entry_to_add->name_len;
+                    cur_entry->file_type = entry_to_add->file_type;
+                    cur_entry->rec_len = EXT2_BLOCK_SIZE - cur_rec;
+                    strncpy(cur_entry->name, entry_to_add->name, cur_entry->name_len);
+                    entry_inserted = 1;
+                }                
+            }else{
+                    cur_entry->rec_len = calculate_reclen(cur_entry);
+                    cur_rec += cur_entry->rec_len;
+                    cur_entry = (struct ext2_dir_entry *)(disk + EXT2_BLOCK_SIZE*block_index + cur_rec);
+                    cur_entry->inode = entry_to_add->inode;
+                    cur_entry->name_len = entry_to_add->name_len;
+                    cur_entry->file_type = entry_to_add->file_type;
+                    cur_entry->rec_len = EXT2_BLOCK_SIZE - cur_rec;
+                    strncpy(cur_entry->name, entry_to_add->name, cur_entry->name_len);
+                    entry_inserted = 1;
+            }
+    }
+    return 0;
+}
 
 
 
@@ -316,6 +577,16 @@ int allocate_dblock(){
 	return new_block;
 }
 
+/* Increase number of free blocks. */
+void increase_free_blocks(){
+    sb->s_free_blocks_count += 1;
+	gd->bg_free_blocks_count += 1;	
+}
+
+void increase_free_inodes(){
+    sb->s_free_inodes_count += 1;
+	gd->bg_free_inodes_count += 1;
+}
 
 /* Given a path, compute the number of levels of the given path.
  * note that path should be in this form a/b (retrive the pre , back trailing '/'s).
